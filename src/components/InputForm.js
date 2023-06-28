db3796ab4764d4dbe1948dfa98081e317d3f3984
david <tochukwud750@gmail.com>
2023-06-28 09:48:11 +0100

    just made the search bar to be ok

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -14,10 +14,10 @@ function InputForm() {
     value={searchTerm}
     onChange={(e) => setSearchTerm(e.target.value)}
     type="text" placeholder='search movies' className='outline-none text-sm p-3 h-8 rounded-l-lg text-black'/>
-     <button className='bg-[#3434bb] w-[40px] rounded-r-lg'>
-    
-     <svg 
+     <button className='bg-[#3434bb] w-[40px] rounded-r-lg'
      onClick={()=> { setSearchTerm(searchTerm); fetchMovie(searchTerm); setSearchTerm('')}}
+     >
+     <svg 
      className='text-lg ml-2' fill='white'
          height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></button>
 </div>
